903ecea60ff9ea852eb13572e6942bc9f5a2de51
hassanjaber2022 <107345159+hassanjaber2022@users.noreply.github.com>
2022-11-05 14:10:36 +0300

    done!

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+function BMI(weight, height) {
+    let result = weight / (height * height);
+    return result;
+}
+
+console.log(BMI(50, 1.7));
+
+function Status(BodyMass) {
+if (BodyMass < 18.5) {
+  return "💔 لديك نقص في الوزن";
+} else if (BodyMass >= 18.5 || BodyMass <= 25) {
+  return "وزنك صحي 😍";
+} else {
+  return"😔 لديك زيادة في الوزن";
+}
+}
+function calculate() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+    let bmi_value = BMI(weight, height);
+    let desc = Status(bmi_value);
+    let box = document.getElementById("result");
+    box.innerText = bmi_value + " == " + desc;
+}
+
